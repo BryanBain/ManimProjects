f467e94728da61ca39ee2bb449116549e1232808
bryanbain <baintrain81@att.net>
2021-08-04 22:26:00 -0400

    through Example 1c in Simplifying Rationals

--- simplifyRationals.py
+++ simplifyRationals.py
@@ -29,13 +29,69 @@ class SimplifyRationals(Scene):
 		self.play(ReplacementTransform(ex1, ex1_1))
 		self.wait()
 		ex1_1a = MathTex("\\frac{(x+1)(x+3)}{x+1}").scale(2)
-		self.play(FadeToColor(ex1_1a[0][1:4], YELLOW))
-		self.play(FadeToColor(ex1_1a[0][11:14], YELLOW))
+		num_common_factor_1_1a = ex1_1a[0][1:4]
+		den_common_factor_1_1a = ex1_1a[0][11:14]
+		common_factors_1_1a = VGroup(num_common_factor_1_1a, den_common_factor_1_1a)
+		self.play(FadeToColor(common_factors_1_1a, YELLOW, run_time=0.01))
 		self.play(ReplacementTransform(ex1_1, ex1_1a))
 		ex1_2 = MathTex("\\frac{1(x+3)}{1}").scale(2)
 		self.play(ReplacementTransform(ex1_1a, ex1_2))
-		self.play(FadeToColor(ex1_2[0][0], YELLOW))
-		self.play(FadeToColor(ex1_2[0][-1], YELLOW))
+		num_common_factor1_2 = ex1_2[0][0]
+		den_common_factor1_2 = ex1_2[0][-1]
+		common_factors1_2 = VGroup(num_common_factor1_2, den_common_factor1_2)
+		self.play(FadeToColor(common_factors1_2, YELLOW, run_time=0.01))
 		self.wait()
 		ex1_3 = MathTex("(x+3)").scale(2)
 		self.play(ReplacementTransform(ex1_2, ex1_3))
+		self.play(FadeOut(ex1_3))
+
+		ex2 = MathTex("\\frac{x^2+7x+10}{x+2}").scale(2)
+		self.play(FadeIn(ex2), run_time = 2)
+		self.wait()
+		ex2_1 = MathTex("\\frac{(x+2)(x+5)}{x+2}").scale(2)
+		self.play(ReplacementTransform(ex2, ex2_1))
+		self.wait()
+		num_common_factor_2_1 = ex2_1[0][1:4]
+		den_common_factor_2_1 = ex2_1[0][11:14]
+		common_factors_2_1 = VGroup(num_common_factor_2_1, den_common_factor_2_1)
+		self.play(FadeToColor(common_factors_2_1, YELLOW, run_time=0.01))
+		self.wait()
+		ex2_2 = MathTex("\\frac{1(x+5)}{1}").scale(2)
+		self.play(ReplacementTransform(ex2_1, ex2_2))
+		self.wait()
+		num_common_factor_2_2 = ex2_2[0][0]
+		den_common_factor_2_2 = ex2_2[0][-1]
+		common_factors_2_2 = VGroup(num_common_factor_2_2, den_common_factor_2_2)
+		self.play(FadeToColor(common_factors_2_2, YELLOW, run_time=0.01))
+		# self.play(FadeToColor(ex2_2[0][0], YELLOW, run_time=0.01))
+		# self.play(FadeToColor(ex2_2[0][-1], YELLOW, run_time=0.01))
+		self.wait()
+		ex2_3 = MathTex("x+5").scale(2)
+		self.play(ReplacementTransform(ex2_2, ex2_3))
+		self.play(FadeOut(ex2_3))
+
+		ex3 = MathTex("\\frac{x^2-7x-18}{2x^2+3x-2}").scale(2)
+		self.play(FadeIn(ex3), run_time=2)
+		self.wait()
+		ex3_1 = MathTex("\\frac{(x-9)(x+2)}{(2x-1)(x+2)}").scale(2)
+		self.play(ReplacementTransform(ex3, ex3_1))
+		self.wait()
+		num_common_factor_3_1 = ex3_1[0][6:9]
+		den_common_factor_3_1 = ex3_1[0][18:21]
+		common_factors_3_1 = VGroup(num_common_factor_3_1, den_common_factor_3_1)
+		self.play(FadeToColor(common_factors_3_1, YELLOW, run_time=0.01))
+		self.wait()
+		ex3_2 = MathTex("\\frac{(x-9)1}{(2x-1)1}").scale(2)
+		self.play(ReplacementTransform(ex3_1, ex3_2))
+		self.wait()
+
+		num_common_factor_3_2 = ex3_2[0][5]
+		den_common_factor_3_2 = ex3_2[0][-1]
+		common_factors_3_2 = VGroup(num_common_factor_3_2, den_common_factor_3_2)
+		self.play(FadeToColor(common_factors_3_2, YELLOW, run_time=0.01))
+		# self.play(FadeToColor(ex2_2[0][0], YELLOW, run_time=0.01))
+		# self.play(FadeToColor(ex2_2[0][-1], YELLOW, run_time=0.01))
+		self.wait()
+		ex3_3 = MathTex("\\frac{x-9}{2x-1}").scale(2)
+		self.play(ReplacementTransform(ex3_2, ex3_3))
+		self.play(FadeOut(ex3_3))
