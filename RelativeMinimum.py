7ba131f23fd7e40ef050483298ef07be45ccd4a1
bryanbain <baintrain81@att.net>
2021-08-21 12:41:06 -0400

    added text to FunctionExtrema and RelativeMinimum

--- RelativeMinimum.py
+++ RelativeMinimum.py
@@ -1,5 +1,6 @@
 from manim import *
 import numpy as np 
+import math
 
 class RelativeMinimum(Scene):
 	def construct(self):
@@ -20,15 +21,24 @@ class RelativeMinimum(Scene):
 		dot = Dot(point = initial_point, color=YELLOW)
 		dot.add_updater(lambda x: x.move_to(ax.c2p(t.get_value(), func(t.get_value()))))
 
-		# x_space = np.linspace(*ax.x_range[0:40],200)
 		x_space = np.linspace(-1.387,3,num=100)
 
 		max_index = func(x_space).argmin()
 
 		text = Text("Relative minimum", color=YELLOW).scale(0.75)
+		text1 = MathTex("(0.721, -2.268)", color=YELLOW).scale(1)
 		text.next_to([ax.coords_to_point(0.721,-2.268)], DOWN)
+		text1.next_to(text, DOWN)
+
+		# y_value = DecimalNumber(0, num_decimal_places=4, color=YELLOW)
+		# y_value.add_updater(lambda d: d.next_to(dot, UP))
+		# y_value.add_updater(lambda d: d.set_value(func(t.get_value())))
 
 		self.add(ax, graph, dot)
 		self.play(t.animate.set_value(x_space[max_index]), run_time=3)
 		self.add(text)
 		self.wait()
+		self.add(text1)
+		self.wait()
+
+
--- functionExtrema.py
+++ functionExtrema.py
@@ -20,15 +20,18 @@ class FunctionExtrema(Scene):
 		dot = Dot(point = initial_point, color=YELLOW)
 		dot.add_updater(lambda x: x.move_to(ax.c2p(t.get_value(), func(t.get_value()))))
 
-		# x_space = np.linspace(*ax.x_range[0:40],200)
 		x_space = np.linspace(-3,-1,num=100)
 
 		max_index = func(x_space).argmax()
 
 		text = Text("Relative maximum", color=YELLOW).scale(0.75)
 		text.next_to([ax.coords_to_point(-1.387,2.417)], UP)
+		text1 = MathTex("(-1.387, 2.417)", color=YELLOW).scale(1)
+		text1.next_to(text, UP)
 
 		self.add(ax, graph, dot)
 		self.play(t.animate.set_value(x_space[max_index]), run_time=3)
 		self.add(text)
+		self.wait()
+		self.add(text1)
 		self.wait()
