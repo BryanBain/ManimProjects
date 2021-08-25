619250d56d55028411114e5139848ce130443551
bryanbain <baintrain81@att.net>
2021-08-25 10:12:32 -0400

    added intro text

--- averageRateChange.py
+++ averageRateChange.py
@@ -2,6 +2,28 @@ from manim import *
 
 class AverageRateOfChange(Scene):
     def construct(self):
+
+        text_start = Text("The average rate of change is the slope \n" \
+        "of the line between 2 points on a curve.",
+            t2c={'[3:22]': ORANGE, '[27:32]': BLUE},
+            t2w={'slope': BOLD})
+
+        self.play(Write(text_start), run_time=3)
+        self.wait()
+        self.remove(text_start)
+
+        text_example = Text("For example, in the interval [0, 3],\n" \
+            "find the average rate of change of the function"
+            )
+        text_function = MathTex(r"\\f(x) = 0.5(x-1)^2+1").scale(1.5)
+        # text_example1 = Text("in the interval [0, 3]")
+
+        group = VGroup(text_example, text_function).arrange(DOWN)
+
+        self.play(Write(group), run_time = 3)
+        self.wait(2)
+        self.remove(group)
+
         axes = Axes(
                 x_range = [-2,4,1],
                 y_range = [0,4,1],
