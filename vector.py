671a0c3642e06c16b6b801df7af30e45ee90f016
bryanbain <baintrain81@att.net>
2021-11-14 10:52:22 -0500

    need to film

--- vector.py
+++ vector.py
@@ -8,36 +8,62 @@ class IntroVectors(Scene):
 		definition1[1].set_color(RED)
 		definition2 = Tex(r"a line segment with direction.").next_to(definition1, DOWN)
 		definition = VGroup(definition1, definition2)
-		self.play(Write(definition))
-		self.wait()
+		self.play(Write(definition, run_time=2))
+		self.wait(2)
 		self.play(Uncreate(title))
 		self.play(Uncreate(definition))
+
+		plane = NumberPlane().add_coordinates()
+		vec1 = Line(start=plane.c2p(0,0), end=plane.c2p(2,3), color=YELLOW).add_tip()
+		vec2 = Line(start=plane.c2p(-2,-3), end=plane.c2p(4,1), color=BLUE).add_tip()
+		vec3 = Line(start=plane.c2p(-1,1), end=plane.c2p(-1,0), color=RED).add_tip()
+		self.play(Create(plane))
+		self.wait()
+		self.play(Create(vec1))
+		self.play(Create(vec2))
+		self.play(Create(vec3))
+		self.wait(3)
+		self.remove(plane, vec1, vec2, vec3)
+		self.wait()
+
 		placement1 = Tex(r"We can start a vector anywhere,")
 		placement2 = Tex(r"but we usually draw it from the origin.").next_to(placement1, DOWN)
 		placement = VGroup(placement1, placement2).shift(2*UP)
 		self.play(Write(placement))
 		self.wait()
 		self.play(Uncreate(placement))
+
 		example = Tex(r"For instance, suppose we want to graph")
 		self.play(Write(example))
 		v = Matrix([[3],[4]])
 		vec_tex = Tex(r"$\vec{v} = $").next_to(v, LEFT)
 		vec1 = VGroup(vec_tex, v).next_to(v, DOWN)
 		self.play(Write(vec1))
+		self.wait(3)
+		v_x_box = SurroundingRectangle(v[0][0])
+		v_y_box = SurroundingRectangle(v[0][1])
+		self.play(Create(v_x_box))
+		self.wait()
+		self.remove(v_x_box)
+		self.wait()
+		self.play(Create(v_y_box))
+		self.wait()
+		self.remove(v_y_box)
 		self.wait()
 		self.remove(example, vec1)
 		self.wait()
 
-
-		plane = NumberPlane(x_range=(-2,5), y_range=(-2,5)).add_coordinates().to_edge(LEFT)
-		self.play(Create(plane))
-		self.wait(3)
-		intro_vec = plane.get_vector([3,4], color=YELLOW)
-		self.play(Create(intro_vec, run_time=2))
 		v = Matrix([[3],[4]])
 		vec_tex = Tex(r"$\vec{v} = $").next_to(v, LEFT)
 		vec1.to_edge(UP+RIGHT, buff=1).set_color(YELLOW)
 		self.play(Write(vec1))
+		plane = NumberPlane(x_range=(-2,5), y_range=(-2,5)).add_coordinates().to_edge(LEFT)
+		self.play(Create(plane))
+		self.wait()
+		intro_vec = plane.get_vector([3,4], color=YELLOW)
+		self.play(Create(intro_vec, run_time=2))
+		self.wait()
+		
 
 class VectorAddition(VectorScene):
 	def construct(self):
@@ -52,8 +78,9 @@ class VectorAddition(VectorScene):
 		vec_w = VGroup(w_tex, w).next_to(vec_v, 2*RIGHT)
 		vec_group = VGroup(vec_v, vec_w)
 		self.play(Write(vec_group))
-		# self.wait()
+		self.wait()
 		self.play(vec_group.animate.shift(1.5*UP))
+		self.wait()
 
 		addition = Tex(r"$\vec{v} + \vec{w}$")
 		self.play(Write(addition))
@@ -69,6 +96,7 @@ class VectorAddition(VectorScene):
 		self.play(Write(resultant))
 
 		self.remove(title, vec_group, addition, vec_group2, resultant, equals)
+		self.wait()
 
 		plane = NumberPlane(x_range = (-2,6), y_range=(-4,5)).add_coordinates().to_edge(LEFT)
 		self.add(plane)
@@ -82,9 +110,10 @@ class VectorAddition(VectorScene):
 		self.wait()
 		w_vector = Arrow(plane.c2p(0,0), plane.c2p(2,-3), buff=0, color=BLUE)
 		self.play(Create(w_vector))
+		self.wait(2)
 		self.play(w_vector.animate.shift(3*RIGHT, 4*UP))
 		resultant_vec = Arrow(plane.c2p(0,0), plane.c2p(5,1), buff=0 , color=ORANGE)
-		self.play(Create(resultant_vec))
+		self.play(Create(resultant_vec, run_time=2))
 		resultant.set_color(ORANGE).next_to(vec_w, DOWN)
 		resultant_tex = Tex(r"$\vec{v}+\vec{w}=$", color=ORANGE).next_to(resultant, LEFT)
 		self.play(Write(resultant_tex))
@@ -93,10 +122,12 @@ class VectorAddition(VectorScene):
 
 class VectorScaling(Scene):
 	def construct(self):
+		title = Title("Scalar Multiplication")
+		self.play(Write(title))
 		scalar_def = Text("A scalar is another name for a real number.", t2c={' scala': BLUE})
-		self.play(Write(scalar_def))
-		self.remove(scalar_def)
+		self.play(Write(scalar_def, run_time=2))
 		self.wait()
+		self.remove(scalar_def, title)
 
 		scalar_explanation = Tex("When we mutliply a vector by a scalar,")
 		scalar_explanation2 = Tex("we multiply both coordinate values by the scalar.").next_to(scalar_explanation, DOWN)
@@ -123,14 +154,13 @@ class VectorScaling(Scene):
 		self.wait()
 		self.play(Write(a_vec_lbl))
 		self.wait()
-		self.play(Transform(a_group, dbl_group))
+		self.play(Transform(a_group, dbl_group, run_time=2))
 		self.wait()
 		self.remove(a_group, dbl_group, plane)
 		self.wait()
 
 		plane2 = NumberPlane(x_range=[-5,4,1], y_range=[-3,3,1]).add_coordinates().to_edge(LEFT)
 		self.play(Create(plane2))
-		self.wait()
 
 		a_vec = Line(plane2.c2p(0,0), plane2.c2p(3,-2), color=YELLOW).add_tip()
 		a_vec_name = Tex(r"$\vec{a}$", color=YELLOW).next_to(a_vec, RIGHT)
@@ -144,28 +174,26 @@ class VectorScaling(Scene):
 			color=ORANGE).shift(4*RIGHT)
 		neg_group = VGroup(neg_vec, neg_vec_name, neg_vec_lbl)
 
-		self.play(Create(a_vec))
-		self.play(Write(a_vec_name))
-		self.wait()
-		self.play(Write(a_vec_lbl))
+		self.play(Create(a_group))
 		self.wait()
 		self.play(Transform(a_group, neg_group))
-		self.wait()
+		self.wait(2)
 		self.remove(a_group, neg_group, plane2)
 		self.wait()
 
 		example_text = Tex(r"Given $\vec{v}=\begin{bmatrix} 1 \\ -1 \end{bmatrix}$ and "
-			r"$\vec{w}=\begin{bmatrix} -2 \\ 0 \end{bmatrix}$, find $3\vec{v}+\vec{w}$")
+			r"$\vec{w}=\begin{bmatrix} -2 \\ 0 \end{bmatrix}$, find $3\vec{v} + \vec{w}$")
 		self.play(Write(example_text))
 		self.wait()
 		self.remove(example_text)
+		self.wait()
 
 		example_plane = NumberPlane(x_range=[-4,4,1], 
 			y_range=[-4,4,1]).add_coordinates().to_edge(LEFT)
 		v_example = Line(start=example_plane.c2p(0,0), 
 			end=example_plane.c2p(1,-1), color=YELLOW).add_tip()
-		v_example_lbl = Tex(r"$\vec{v}$", color=YELLOW).next_to(v_example.get_center())
-		v_example_tex = Tex(r"$\vec{v}=\begin{bmatrix} 1 \\ -1 \end{bmatrix}$").shift(3.5*RIGHT + 2*UP)
+		v_example_lbl = Tex(r"$\vec{v}$", color=YELLOW).next_to(v_example, RIGHT)
+		v_example_tex = Tex(r"$\vec{v}=\begin{bmatrix} 1 \\ -1 \end{bmatrix}$", color=YELLOW).shift(3.5*RIGHT + 2*UP)
 		self.play(Create(example_plane))
 		self.play(Create(v_example))
 		self.play(Write(v_example_lbl))
@@ -174,7 +202,7 @@ class VectorScaling(Scene):
 		w_example = Line(start=example_plane.c2p(0,0), end=example_plane.c2p(-2,0),
 			color=BLUE).add_tip()
 		w_example_lbl = Tex(r"$\vec{w}$", color=BLUE).next_to(w_example, UP)
-		w_example_tex = Tex(r"$\vec{w}=\begin{bmatrix} -2 \\ 0 \end{bmatrix}$").next_to(v_example_tex, RIGHT)
+		w_example_tex = Tex(r"$\vec{w}=\begin{bmatrix} -2 \\ 0 \end{bmatrix}$", color=BLUE).next_to(v_example_tex, RIGHT)
 		self.play(Create(w_example))
 		self.play(Write(w_example_lbl))
 		self.play(Write(w_example_tex))
@@ -182,7 +210,7 @@ class VectorScaling(Scene):
 		scale_v_example = Line(start=example_plane.c2p(0,0), 
 			end=example_plane.c2p(3,-3), color=YELLOW).add_tip()
 		scale_v_lbl = Tex(r"$3\vec{v}$", color=YELLOW).next_to(scale_v_example)
-		scale_v_tex = Tex(r"$3\vec{v} = \begin{bmatrix} 3 \\ -3 \end{bmatrix}$").next_to(w_example_tex, LEFT)
+		scale_v_tex = Tex(r"$3\vec{v} = \begin{bmatrix} 3 \\ -3 \end{bmatrix}$", color=YELLOW).next_to(w_example_tex, LEFT)
 		self.play(Transform(v_example, scale_v_example))
 		self.play(Transform(v_example_lbl, scale_v_lbl))
 		self.play(Transform(v_example_tex, scale_v_tex))
@@ -193,8 +221,67 @@ class VectorScaling(Scene):
 
 		vec_sum = Line(start=example_plane.c2p(0,0), end=example_plane.c2p(1,-3),
 			color=PURPLE).add_tip()
-		vec_sum_lbl = Tex(r"$\vec{v}+\vec{w}$", color=PURPLE).next_to(vec_sum, LEFT)
+		vec_sum_lbl = Tex(r"$3\vec{v}+\vec{w}$", color=PURPLE).next_to(vec_sum, LEFT)
 		vec_sum_tex = Tex(r"$3\vec{v} + \vec{w} = \begin{bmatrix} 1 \\ -3 \end{bmatrix}$", color=PURPLE).next_to(scale_v_tex, 2*DOWN)
+		self.wait()
+		self.play(Create(vec_sum))
+		self.play(Write(vec_sum_lbl))
+		self.play(Write(vec_sum_tex))
+		self.wait()
+		self.remove(vec_sum, vec_sum_lbl, vec_sum_tex, v_example, v_example_lbl, v_example_tex,
+		w_example, scale_v_lbl, scale_v_tex, example_plane,
+		example_resultant, scale_v_example, w_example_lbl, w_example_tex)
+
+		#######################################
+
+		example_text = Tex(r"Given $\vec{v}=\begin{bmatrix} 1 \\ -1 \end{bmatrix}$ and "
+			r"$\vec{w}=\begin{bmatrix} -2 \\ 0 \end{bmatrix}$, find $3\vec{v}-\vec{w}$")
+		self.play(Write(example_text))
+		self.wait()
+		self.remove(example_text)
+		self.wait()
+
+		example_plane = NumberPlane(x_range=[-3,5.5,1], 
+			y_range=[-4,4,1]).add_coordinates().to_edge(LEFT)
+		v_example = Line(start=example_plane.c2p(0,0), 
+			end=example_plane.c2p(1,-1), color=YELLOW).add_tip()
+		v_example_lbl = Tex(r"$\vec{v}$", color=YELLOW).next_to(v_example, RIGHT)
+		v_example_tex = Tex(r"$\vec{v}=\begin{bmatrix} 1 \\ -1 \end{bmatrix}$", color=YELLOW).shift(3.5*RIGHT + 2*UP)
+		self.play(Create(example_plane))
+		self.play(Create(v_example))
+		self.play(Write(v_example_lbl))
+		self.play(Write(v_example_tex))
+
+		w_example = Line(start=example_plane.c2p(0,0), end=example_plane.c2p(-2,0),
+			color=BLUE).add_tip()
+		w_example_lbl = Tex(r"$\vec{w}$", color=BLUE).next_to(w_example, DOWN)
+		w_example_tex = Tex(r"$\vec{w}=\begin{bmatrix} -2 \\ 0 \end{bmatrix}$", color=BLUE).next_to(v_example_tex, RIGHT)
+		self.play(Create(w_example))
+		self.play(Write(w_example_lbl))
+		self.play(Write(w_example_tex))
+		w_example_lbl.add_updater(lambda x: x.next_to(w_example, DOWN))
+		scale_v_example = Line(start=example_plane.c2p(0,0), 
+			end=example_plane.c2p(3,-3), color=YELLOW).add_tip()
+		scale_v_lbl = Tex(r"$3\vec{v}$", color=YELLOW).next_to(scale_v_example.get_center())
+		scale_v_tex = Tex(r"$3\vec{v} = \begin{bmatrix} 3 \\ -3 \end{bmatrix}$", color=YELLOW).next_to(w_example_tex, LEFT)
+		self.play(Transform(v_example, scale_v_example))
+		self.play(Transform(v_example_lbl, scale_v_lbl))
+		self.play(Transform(v_example_tex, scale_v_tex))
+		self.remove(v_example_lbl)
+		scale_w_example = Line(start=example_plane.c2p(0,0),
+		end=example_plane.c2p(2,0), color=BLUE).add_tip()
+		self.play(Transform(w_example, scale_w_example))
+		self.wait()
+		example_resultant = Line(start=example_plane.c2p(3,-3), end=example_plane.c2p(5,-3),
+			color=BLUE).add_tip()
+		self.play(Transform(w_example, example_resultant))
+		self.remove(w_example_lbl)
+		self.wait()
+
+		vec_sum = Line(start=example_plane.c2p(0,0), end=example_plane.c2p(5,-3),
+			color=PURPLE).add_tip()
+		vec_sum_lbl = Tex(r"$3\vec{v} - \vec{w}$", color=PURPLE).next_to(vec_sum.get_center(), 1.25*RIGHT)
+		vec_sum_tex = Tex(r"$3\vec{v} - \vec{w} = \begin{bmatrix} 5 \\ -3 \end{bmatrix}$", color=PURPLE).to_edge(RIGHT)
 		self.wait()
 		self.play(Create(vec_sum))
 		self.play(Write(vec_sum_lbl))
@@ -294,10 +381,12 @@ class Matrices(Scene):
 		elements_def.set_color_by_tex('lement',ORANGE)
 		self.play(Write(elements_def))
 		self.wait()
+		a_12box = SurroundingRectangle(m[0][1])
+		self.play(Create(a_12box))
 		element_example = Tex(r"$a_{12}=6$").next_to(elements_def, 2*DOWN)
 		self.play(Write(element_example))
 		self.wait()
-		self.remove(matrix_example, elements_def, element_example)
+		self.remove(matrix_example, elements_def, element_example, a_12box)
 
 		vec_matrix1 = Tex(r"We can think of a matrix as ")
 		vec_matrix2 = Tex(r"several vectors in the coordinate plane.").next_to(vec_matrix1, DOWN)
@@ -347,12 +436,14 @@ class Matrices(Scene):
 
 class MatrixAddition(Scene):
 	def construct(self):
+		title = Title("Matrix Addition")
+		self.play(Write(title))
 		intro_text1 = Tex(r"We can add or subtract two matrices")
 		intro_text2 = Tex(r"as long as the have the same dimensions.").next_to(intro_text1, DOWN)
 		intro_text = VGroup(intro_text1, intro_text2)
 		self.play(Write(intro_text))
 		self.wait()
-		self.remove(intro_text)
+		self.remove(intro_text, title)
 
 		plane = NumberPlane(x_range=[-4,4,1], y_range=[-4,4,1],
 			x_length = 8, y_length = 5).add_coordinates().to_edge(DOWN)
@@ -504,9 +595,9 @@ class ScalarMult(Scene):
 		self.remove(j_box)
 		self.wait()
 
-		neg_d_UL = negative_d.get_entries()[0].to_edge(RIGHT + DOWN)
-		self.play(Write(neg_d_UL))
-		self.wait()
+		# neg_d_UL = negative_d.get_entries()[0].to_edge(RIGHT + DOWN)
+		# self.play(Write(neg_d_UL))
+		# self.wait()
 
 
 
