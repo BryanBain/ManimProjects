2a51670dc74276603d49921fd6d40a457c2df928
bryanbain <baintrain81@att.net>
2022-07-08 13:47:55 -0400

    added cosine graph generator

--- trig_graphs.py
+++ trig_graphs.py
@@ -1,10 +1,20 @@
 from manim import *
 
 def get_vertical_line_for_circle(x,y, width, color):
-    # Draws vertical line connecting the point on the circle to the circle's x-axis
     result = VGroup()
     line = DashedLine(
         start = [x,0,0],
+        end = [x,y,0],
+        stroke_width = width,
+        stroke_color = color
+    )
+    result.add(line)
+    return result
+
+def get_horiz_line_for_circle(x,y, width, color):
+    result = VGroup()
+    line = DashedLine(
+        start = [-5.11111111,y,0],
         end = [x,y,0],
         stroke_width = width,
         stroke_color = color
@@ -34,8 +44,6 @@ class SineCurveUnitCircle(Scene):
             for t,x in zip(x_labels, np.arange(PI/2, 4*PI+PI/2, PI/2))
         ])
 
-        self.curve_start = axes.get_origin()
-
         self.play(Create(axes))
         self.play(Create(x_tex_labels))
         self.wait()
@@ -49,11 +57,10 @@ class SineCurveUnitCircle(Scene):
         self.play(Create(axis_grp.to_edge(LEFT)))
         self.wait()
 
-        t = ValueTracker(0)     # Set the value to trace the function
-        p1 = circle.point_at_angle(0 + t.get_value())
+        t = ValueTracker(0)     # Set the value to trace the function starting at 0 radians
+        p1 = circle.point_at_angle(t.get_value())
 
         dot = Dot(p1, color=YELLOW)
-        x = dot.get_center()[0]
 
         # Use the t.get_value() in the x_range to trace out the graph
         sine_func = always_redraw(
@@ -70,6 +77,67 @@ class SineCurveUnitCircle(Scene):
             t.animate.set_value(4*PI),
             Rotate(dot, angle = 4*PI, about_point=circle.get_center()),
             Create(sine_func),
+            run_time = 15,
+            rate_func = linear
+        )
+        self.wait()
+
+class CosineCurveUnitCircle(Scene):
+    def construct(self):
+        title = Tex(r"$f(x) = \cos(x)$").to_edge(UR)
+        self.play(Write(title))
+
+        # Create the axis to plot the function on
+        x_labels = ["\\tfrac{\pi}{2}", "\pi",
+            "\\tfrac{3\pi}{2}", "2\pi",
+            "\\tfrac{5\pi}{2}", "3\pi",
+            "\\tfrac{7\pi}{2}", "4\pi"]
+        axes = Axes(
+            x_range = [0,4*PI+PI/2,PI/2],
+            x_length = 10,
+            y_range = [-2,2,1],
+            y_axis_config={"include_numbers": True}
+        ).to_edge(RIGHT, buff=0)
+
+        x_tex_labels = VGroup(*[
+            MathTex(t).next_to(axes.x_axis.n2p(x),0.5*DOWN)
+            for t,x in zip(x_labels, np.arange(PI/2, 4*PI+PI/2, PI/2))
+        ])
+
+        self.play(Create(axes))
+        self.play(Create(x_tex_labels))
+        self.wait()
+
+        # Create the axes for the circle
+        x_axis = Line([-1,0,0], [1,0,0])
+        y_axis = Line([0,-1,0], [0,1,0])
+        circle = Circle(radius = 1)
+        axis_grp = VGroup(x_axis, y_axis, circle).scale(1.5)
+        
+        self.play(Create(axis_grp.to_edge(LEFT)))
+        self.wait()
+
+        t = ValueTracker(0)     # Set the value to trace the function starting at 0 radians
+        p1 = circle.point_at_angle(t.get_value())
+
+        dot = Dot(p1, color=GREEN)
+
+        # Use the t.get_value() in the x_range to trace out the graph
+        cos_func = always_redraw(
+            lambda: axes.plot(
+                lambda x: np.cos(x), x_range=[0,t.get_value()], color=GREEN
+            )
+        )
+       
+        moving_horiz_lines = always_redraw(
+            lambda : get_horiz_line_for_circle(x=dot.get_center()[0],y=dot.get_center()[1], width=4, color=GREEN)
+        )
+
+        self.add(moving_horiz_lines)
+        self.play(
+            t.animate.set_value(4*PI),
+            Rotate(dot, angle = 4*PI, about_point=circle.get_center()),
+            Create(cos_func),
             run_time = 15,
             rate_func = linear
         )
